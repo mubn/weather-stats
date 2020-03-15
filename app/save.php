04168fa4236abd8f5fb47a0b6d60a243614ba1ca
Marek <github@marekurban.de>
2020-03-15 01:06:59 +0100

    Add save programm

---
+++ app/save.php
@@ -0,0 +1,43 @@
+<?php
+
+$pass = "xyz123";
+$dbname = "weather.db";
+
+$sensorid = $_POST["sensorid"];
+$temperature = $_POST["temperature"];
+$pressure = $_POST["pressure"];
+$humidity = $_POST["humidity"];
+
+if (!isset($sensorid) || !is_numeric($sensorid)) {
+    die('Wrong sensor data');
+}
+
+if (!isset($temperature) || !is_numeric($temperature)) {
+    die('Wrong temperature data');
+}
+
+if (!isset($pressure) || !is_numeric($pressure)) {
+    die('Wrong pressure data');
+}
+
+if (!isset($humidity) || !is_numeric($humidity)) {
+    die('Wrong humidity data');
+}
+
+$password = $_POST["password"];
+if (!isset($password) || !($password == $pass)) {
+    die('Wrong password');
+}
+
+$db = new SQLite3($dbname);
+
+$db->exec("INSERT INTO 
+    sensor_values(
+        sensorid,
+        temperature,
+        pressure,
+        humidity
+    ) VALUES($sensorid,$temperature,$pressure,$humidity)"
+);
+
+echo "SUCCESS";
