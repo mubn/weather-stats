e169c64cf4e0104fe2de5c6c0a168140b76fd7c7
Marek <github@marekurban.de>
2021-01-11 19:39:02 +0100

    Set basic auth headers on login failure

--- app/login.php
+++ app/login.php
@@ -3,10 +3,15 @@
 $user     = getenv('APP_USER');
 $pass     = getenv('APP_PASS');
 
-if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
+function setBasicAuthHeader(){
     header('WWW-Authenticate: Basic realm="My Realm"');
     header('HTTP/1.0 401 Unauthorized');
+}
+
+if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
+    setBasicAuthHeader();
     die('You need to log in.');
 } else if ($_SERVER['PHP_AUTH_USER'] != $user || $_SERVER['PHP_AUTH_PW'] != $pass) {
+    setBasicAuthHeader();
     die('Your credentials do not match.');
 }
