736ef2685f66d2392fafb5064b13f36541c1dea4
Marek <github@marekurban.de>
2021-02-04 22:03:40 +0100

    Improve the liveness probe

--- app/live.php
+++ app/live.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "ok";
+$db = new SQLite3('/db/weather.db');
+
+$res = $db->query("SELECT * FROM sensor_values LIMIT 1");
+
+if ( $res->fetchArray(SQLITE3_ASSOC) ) {
+  header('HTTP/1.0 200 OK');
+} else {
+  header('HTTP/1.0 500 Internal Server Error');
+}
