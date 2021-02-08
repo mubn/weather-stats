71fb2cc8c18f6d2801a4a214812677f81dfc376c
Marek <github@marekurban.de>
2021-02-09 00:07:36 +0100

    Fix and simplify liveness test

--- app/live.php
+++ app/live.php
@@ -1,11 +1,6 @@
 <?php
-
-$db = new SQLite3('/db/weather.db');
-
-$res = $db->query("SELECT * FROM sensor_values LIMIT 1");
-
-if ( $res->fetchArray(SQLITE3_ASSOC) ) {
-  header('HTTP/1.0 200 OK');
-} else {
+try {
+  $db = new SQLite3('/db/weather.db');
+} catch (Exception e) {
   header('HTTP/1.0 500 Internal Server Error');
 }
